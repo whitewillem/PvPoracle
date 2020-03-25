fbc163807e219468faff8b864313786564155381
whitewillem <jezuswateengedoe@gmail.com>
2020-03-25 21:24:39 +0100

    add wigglytuff_25.sql

---
+++ great league/rank1-25/wigglytuff_25.sql
@@ -0,0 +1,206 @@
+/* ------------------------------------ */
+/* Great League Jigglypuff, Wigglytuff Rank 1-25 */
+/* ------------------------------------ */
+
+/* Jigglypuff */
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 12, 14, 4, 0, 9000000, 0, 12, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 12, 15, 4, 0, 9000000, 0, 12, 15, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 31, 0, 13, 8, 4, 0, 9000000, 0, 13, 8, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 13, 12, 4, 0, 9000000, 0, 13, 12, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 13, 13, 4, 0, 9000000, 0, 13, 13, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 31, 0, 14, 5, 4, 0, 9000000, 0, 14, 5, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 9, 4, 0, 9000000, 0, 14, 9, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 10, 4, 0, 9000000, 0, 14, 10, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 13, 4, 0, 9000000, 0, 14, 13, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 14, 4, 0, 9000000, 0, 14, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 15, 4, 0, 9000000, 0, 14, 15, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 31, 0, 15, 2, 4, 0, 9000000, 0, 15, 2, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 6, 4, 0, 9000000, 0, 15, 6, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 7, 4, 0, 9000000, 0, 15, 7, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 10, 4, 0, 9000000, 0, 15, 10, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 11, 4, 0, 9000000, 0, 15, 11, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 12, 4, 0, 9000000, 0, 15, 12, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 13, 14, 4, 0, 9000000, 1, 13, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 14, 10, 4, 0, 9000000, 1, 14, 10, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 14, 11, 4, 0, 9000000, 1, 14, 11, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 15, 8, 4, 0, 9000000, 1, 15, 8, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 29, 2, 15, 14, 4, 0, 9000000, 2, 15, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 29, 2, 15, 15, 4, 0, 9000000, 2, 15, 15, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 29, 3, 14, 14, 4, 0, 9000000, 3, 14, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 39, 0, 0, 0, 100, 0, 9000, 0, 29, 3, 15, 11, 4, 0, 9000000, 3, 15, 11, 0);
+
+
+/* Wigglytuff */
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 12, 14, 4, 0, 9000000, 0, 12, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 12, 15, 4, 0, 9000000, 0, 12, 15, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 31, 0, 13, 8, 4, 0, 9000000, 0, 13, 8, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 13, 12, 4, 0, 9000000, 0, 13, 12, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 13, 13, 4, 0, 9000000, 0, 13, 13, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 31, 0, 14, 5, 4, 0, 9000000, 0, 14, 5, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 9, 4, 0, 9000000, 0, 14, 9, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 10, 4, 0, 9000000, 0, 14, 10, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 13, 4, 0, 9000000, 0, 14, 13, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 14, 4, 0, 9000000, 0, 14, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 14, 15, 4, 0, 9000000, 0, 14, 15, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 31, 0, 15, 2, 4, 0, 9000000, 0, 15, 2, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 6, 4, 0, 9000000, 0, 15, 6, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 7, 4, 0, 9000000, 0, 15, 7, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 10, 4, 0, 9000000, 0, 15, 10, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 11, 4, 0, 9000000, 0, 15, 11, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 0, 15, 12, 4, 0, 9000000, 0, 15, 12, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 13, 14, 4, 0, 9000000, 1, 13, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 14, 10, 4, 0, 9000000, 1, 14, 10, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 14, 11, 4, 0, 9000000, 1, 14, 11, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 30, 1, 15, 8, 4, 0, 9000000, 1, 15, 8, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 29, 2, 15, 14, 4, 0, 9000000, 2, 15, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 29, 2, 15, 15, 4, 0, 9000000, 2, 15, 15, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 29, 3, 14, 14, 4, 0, 9000000, 3, 14, 14, 0);
+
+INSERT INTO monsters
+(`id`, `ping`, `clean`, `pokemon_id`, `form`, `distance`, `min_iv`, `max_iv`, `min_cp`, `max_cp`, `min_level`, `max_level`, `atk`, `def`, `sta`, `template`, `min_weight`, `max_weight`, `max_atk`, `max_def`, `max_sta`, `gender`)
+VALUES ('CHANNEL_ID_HERE', '', 0, 40, 0, 0, 0, 100, 0, 9000, 0, 29, 3, 15, 11, 4, 0, 9000000, 3, 15, 11, 0);
